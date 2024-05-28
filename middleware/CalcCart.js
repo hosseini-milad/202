24dca23360b9c9b2eb9767f65c3b8e45dcbe1f8a
milad <milad.cia@gmail.com>
2024-05-28 08:28:09 +0330

    cart data

--- middleware/CalcCart.js
+++ middleware/CalcCart.js
@@ -1,13 +1,15 @@
-const cart = require("../models/product/cart")
+const cart = require("../models/product/cart");
+const products = require("../models/product/products");
 
 
 const CalcCart=async(userId)=>{
-    const cartDetail = await cart.find({userId:userId})
+    const cartDetail = (await cart.find({userId:userId})).lean()
     var cartPrice = 0;
     var cartTotal = 0;
     for(var i=0;i<cartDetail.length;i++){
         var price = parseInt(cartDetail[i].price)
-
+        var productDetail = await products.findOne({sku:cartDetail[i].sku})
+        cartDetail[i].productDetail=productDetail
         if(!price){
             cartDetail[i].price = 1250000
             price = 1250000
