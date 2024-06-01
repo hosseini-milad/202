dd5b63cbe26fa491d783f75fce624e55328dd943
milad <milad.cia@gmail.com>
2024-06-01 10:30:06 +0330

    rahkaran

--- middleware/PriceCount.js
+++ middleware/PriceCount.js
@@ -3,7 +3,7 @@ var tax = process.env.TaxRate
 const PriceCount=(price,count)=>{
     if(!price) return(0)
     const priceFloat = 0//parseFloat(price)
-    const count = 0//parseFloat(count)
+    //const count = 0//parseFloat(count)
     const newPrice = priceFloat*count
     return(parseInt(newPrice))
 }
