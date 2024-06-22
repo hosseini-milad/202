715cc6ed031b823ba87ca1345c63d30a39d431ce
milad <milad.cia@gmail.com>
2024-06-22 12:59:58 +0330

    paging error

--- router/esaleApi.js
+++ router/esaleApi.js
@@ -100,7 +100,7 @@ router.post('/list-product',jsonParser,async (req,res)=>{
             { $match:data.sku?{sku:new RegExp('.*' + data.sku + '.*')}:{}},
             { $match:data.unitId?{unitId:data.unitId}:{}},
             { $match:searchCat.length?{catId:{$in:searchCat}}:{}},
-            { $match:(data.active&&data.active=="deactive")?{}:{catId:{$nin:["1","3","4","5"]}}},
+            { $match:(data.active&&data.active=="deactive")?{}:{catId:{$nin:["1","4","5"]}}},
             { $match:data.search?{$or:[
                 {title:new RegExp('.*' + data.search + '.*')},
                 {sku:new RegExp('.*' + data.search + '.*')}
