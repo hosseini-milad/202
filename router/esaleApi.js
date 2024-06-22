9a2318aa4df96b7ff6825b45cad5e50709d9f687
milad <milad.cia@gmail.com>
2024-06-22 10:38:01 +0330

    product detail

--- router/esaleApi.js
+++ router/esaleApi.js
@@ -115,7 +115,7 @@ router.post('/list-product',jsonParser,async (req,res)=>{
             const typeUnique = [...new Set(productList.map((item) => item.category))];
             const categoryList = data.category?await category.find({parent:categoryDetail._id}):
                 await category.find({parent:{$exists:false}})
-           res.status(200).json({data:products,type:typeUnique,
+           res.status(200).json({data:products,type:typeUnique,searchCat:searchCat,
             size:productList.length,success:true,categoryList:categoryList})
     }
     catch(error){
