c945d168f40d3bfbdb98e9d97de18cbb3a4c9d68
m.ilad <hosseini.milad1400@gmail.com>
2024-07-14 12:25:35 +0330

    single

--- router/esaleApi.js
+++ router/esaleApi.js
@@ -104,7 +104,7 @@ router.post('/list-product',jsonParser,async (req,res)=>{
             { $match:data.panel?{}:{active:true}},
             { $match:searchCat.length?{catId:{$in:searchCat}}:{}},
             { $match:(data.active&&data.active=="deactive")?{}:
-                {catId:{$nin:[""]}}},//["1","4","5"]}}},
+                {$and:[{catId:{$exist:true}},{catId:{$nin:[""]}}]}},//["1","4","5"]}}},
             { $match:data.search?{$or:[
                 {title:new RegExp('.*' + data.search + '.*')},
                 {sku:new RegExp('.*' + data.search + '.*')}
