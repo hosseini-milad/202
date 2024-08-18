f08aa69c04d496af2389fa1b1aba45d28132f63b
milad.cia <milad.cia@gmail.com>
2024-08-18 09:27:43 +0330

    fix rules

--- middleware/NormalPrice.js
+++ middleware/NormalPrice.js
@@ -2,12 +2,14 @@
 const NormalPrice=(priceText,count)=>{
         if(!priceText||priceText === null||priceText === undefined) return("")
         if(!count) count = 1
+        var priceNeg = false
+        if(priceText.includes('-')) priceNeg=true
         try{priceText =priceText.split(' ')[0];}catch{}
-        if(priceText === "0"||priceText === 0)return("-");
+        if(priceText === "0"||priceText === 0)return("0");
         var rawPrice = parseInt(priceText.toString().replace(/\D/g,''))*count
         rawPrice = Math.round(rawPrice/1000) * 1000
         //console.log(rawPrice,priceText)
-        return(
+        return((priceNeg?"-":"")+
           (rawPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").replace( /^\D+/g, ''))
         )
 }
