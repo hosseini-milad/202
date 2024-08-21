432a03dae9909b9fa5ae4b68707a13259fcbaa27
milad <milad.cia@gmail.com>
2024-08-21 18:21:19 +0330

    price

--- middleware/CompareValue.js
+++ middleware/CompareValue.js
@@ -1,4 +1,5 @@
 const CompareValue=async(value1,value2)=>{
+    console.log("value1: "+value1+" value2: "+value2)
     if(!value1){
         if(!value2) return true
         if(value2=="0") return true
