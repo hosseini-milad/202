68a005a4d64949e7c9bc520fbb7c9af766f1a233
Amir Hossein Salimi <ah.salimi1431@gmail.com>
2024-09-30 14:02:29 +0330

    sub-menu

--- src/modules/News/NewsTableRow.js
+++ src/modules/News/NewsTableRow.js
@@ -1,14 +1,65 @@
-import React ,{ useState } from "react"
+import React ,{ useState,useEffect,useRef } from "react"
 import Status from "../Components/Status"
 import  env, { normalPriceCount, rxFindCount } from "../../env"
 import NewsQuickDetail from "./NewsComponent/NewsQuickDetail"
+import ErrorAction from "../Components/PopUps/ErrorAction"
 
 function NewsTableRow(props){
   const [openOption,setOpenOption] = useState(0)
   const [checkState,setCheckState] = useState(false)
+  const [Alert,setAlert]=useState(false)
+
   const activeAcc = props.index===props.detail
   console.log(props.data)
   const data=props.data
+  let menuRef = useRef();
+
+  useEffect(() => {
+    let handler = (e)=>{
+      if(!menuRef.current.contains(e.target)){
+        setOpenOption(false);
+        console.log(menuRef.current);
+      }      
+    };
+
+    document.addEventListener("mousedown", handler);
+    
+
+    return() =>{
+      document.removeEventListener("mousedown", handler);
+    }
+
+  });
+  const deleteNews=()=>{
+    //if(newCustomer) {
+      var postOptions={
+          method:'post',
+          headers: {'Content-Type': 'application/json'},
+          body:JSON.stringify({newsCode:data.enTitle})
+        }
+       //console.log(postOptions)
+    fetch(env.siteApi + "/setting/delete-news",postOptions)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        if(result.error){
+          console.log(result.error)
+        }
+          else{
+
+            setTimeout(()=>window.location.href="/news",2000)
+          }
+          
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+  }
+
+
+
+
     return(<React.Fragment>
         <tr 
             className={activeAcc?"activeAccordion":"accordion"}>
@@ -61,30 +112,44 @@ function NewsTableRow(props){
                 <Status status={data.status} class={"order-status"} 
                   lang={props.lang}/>
               </td>
-            <td>
+            <td className="p-r">
               <div className="more-btn">
               <i className={`tableIcon fas ${activeAcc?"fa-chevron-up":"fa-chevron-down"}`} 
                 onClick={()=>props.showDetail(activeAcc?"-1":props.index)} ></i>
-                <i className="tableIcon fas fa-edit" onClick={()=>
-                  window.location.href="/news/detail/"+data.enTitle}></i>
+                {/* <i className="tableIcon fas fa-edit" onClick={()=>
+                  window.location.href="/news/detail/"+data.enTitle}></i> */}
                 <i className="tableIcon fas fa-ellipsis-v" 
                   onClick={()=>setOpenOption(openOption?0:1)}></i>
               </div>
-              {openOption?<div className="sub-more-menu">
-                <div className="sub-option sub-delete">
+              <div className={openOption==true?"sub-more-menu sub-active":"sub-more-menu"} ref={menuRef}>
+                <div className="sub-option sub-delete" onClick={()=>setAlert(true)}>
                 <i className="tableIcon fas fa-remove" style={{color: "#ff0000"}}></i>
                   <p>Delete</p>
                 </div>
-                <div className="sub-option sub-edit">
+                <div className="sub-option sub-edit" onClick={()=>
+                  window.location.href="/news/detail/"+data.enTitle}>
                   <i className="tableIcon fas fa-edit"></i>
                   <p>Edit</p>
                 </div>
-              </div>:<></>}
+              </div>
             </td>
           </tr>
+          {Alert?
+          <ErrorAction 
+          status={"DELETE"} 
+          title={"حذف آیتم"} 
+          text={"آیتم انتخاب شده حذف خواهد شد. آیا مطمئن هستید؟"} 
+          linkText={""} 
+          style={{direction:"rtl"}}
+          buttonText="حذف" 
+          close={()=>setAlert()}
+          action={deleteNews}
+          />:
+          <></>}
           {activeAcc?<tr className="sub-order">
         <td colSpan="9"><NewsQuickDetail data={data}/></td></tr>
           :<React.Fragment></React.Fragment>}
+
           </React.Fragment>
     )
 }
